9240d1df5f35cd305c6a55a9918bede57b053ac5
SauleStan <vogellietuva@gmail.com>
2022-04-11 20:02:30 +0300

    Meeting model

---
+++ MeetingManager/Models/Meeting.cs
@@ -0,0 +1,31 @@
+﻿using MeetingManager.Utils;
+
+namespace MeetingManager.Models
+{
+    public class Meeting
+    {
+        public Guid Id { get; }
+        public string Name { get; set; }
+        public string ResponsiblePerson { get; set; }
+        public string Description { get; set; }
+        public Categories Category { get; set; }
+        public Types Type { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public List<string> People { get; set; }
+
+        public Meeting(string Name, string ResponsiblePerson, string Description, Categories Category, Types Type, DateTime StartDate, DateTime EndDate)
+        {
+            Id = Guid.NewGuid();
+            this.Name = Name;
+            this.ResponsiblePerson = ResponsiblePerson;
+            this.Description = Description;
+            this.Category = Category;
+            this.Type = Type;
+            this.StartDate = StartDate;
+            this.EndDate = EndDate;
+            People = new List<string>();
+        }
+
+    }
+}
---
+++ MeetingManager/Utils/Enums.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeetingManager.Utils
+{
+    public enum Categories
+    {
+        CodeMonkey = 0,
+        Hub = 1,
+        Short = 2,
+        TeamBuilding = 3
+    }
+
+    public enum Types
+    {
+        Live = 0,
+        InPerson = 1
+    }
+}
